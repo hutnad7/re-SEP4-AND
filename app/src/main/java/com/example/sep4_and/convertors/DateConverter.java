4aecae949001218e4df54bb8a078d0732cc50f1f
Andrei Ioanas <ioanasandrei13@gmail.com>
2024-08-10 22:41:20 +0200

    Hand-in

--- app/src/main/java/com/example/sep4_and/convertors/DateConverter.java
+++ app/src/main/java/com/example/sep4_and/convertors/DateConverter.java
@@ -5,6 +5,7 @@ import androidx.room.TypeConverter;
 import java.util.Date;
 
 public class DateConverter {
+
     @TypeConverter
     public static Date fromTimestamp(Long value) {
         return value == null ? null : new Date(value);
