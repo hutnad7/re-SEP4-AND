cf5942183b1d1328ae45cdeb9c1f73340c0226fc
Ion Canariov <ioncnnn@gmail.com>
2024-08-12 08:48:49 +0200

    historicall measurements api

--- app/src/main/java/com/example/sep4_and/network/GreenhouseApi.java
+++ app/src/main/java/com/example/sep4_and/network/GreenhouseApi.java
@@ -1,11 +1,21 @@
 package com.example.sep4_and.network;
 
 import com.example.sep4_and.model.CO2Measurement;
+import com.example.sep4_and.model.HistoricalMeasurement;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface GreenhouseApi {
     @GET("co2_measurements")
     Call<CO2Measurement> getCO2Measurement();
+
+    @GET("historicalMeasurements")
+    Call<List<HistoricalMeasurement>> getHistoricalMeasurements(
+            @Query("startDate") String startDate,
+            @Query("endDate") String endDate
+    );
 }
